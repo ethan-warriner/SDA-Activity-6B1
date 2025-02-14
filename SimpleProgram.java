641f83fd6c757b59802f11d366bd6785655f5787
ethan-warriner <144939320+ethan-warriner@users.noreply.github.com>
2025-02-13 20:58:32 -0500

    Updated SimpleProgram.java

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -2,5 +2,6 @@ public class SimpleProgram {
     public static void main (String[] args) {
         System.out.println("Hello World!");
         System.out.println("3 + 5 = " + (3+5));
+        System.out.println("My name is Ethan.");
     }
 }
